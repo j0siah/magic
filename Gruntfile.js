1cf0c1bb3398213a79bae19d726b1389bd0ae076
Josiah Williams <josiahdwilliams@gmail.com>
2014-06-30 15:15:10 -0400

    changed to work with other library

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		watch: {
+			stylus: {
+				files: [
+				'*.styl'
+				],
+				tasks: ['stylus']
+			}
+		},
+
+		stylus: {
+			compile: {
+				options: {
+					compress: false,
+					paths: ['stylus'],
+					import: [
+						'nib/*'
+					]
+				},
+				files: {
+					'<%= css_output_location %>': ['*.styl']
+				}
+			}
+		},
+
+		'css_output_location': 'magic.css',
+
+	});
+
+	// Load grunt plugins.
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+};
